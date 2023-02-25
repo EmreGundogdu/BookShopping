dbec66d72c9abf6a22bea88cf4eec3d55a75ee45
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-25 21:41:34 +0300

    orderStatus updated

--- gNdgd.UI/Models/OrderStatus.cs
+++ gNdgd.UI/Models/OrderStatus.cs
@@ -5,6 +5,8 @@ namespace gNdgd.UI.Models
     public class OrderStatus
     {
         public int Id { get; set; }
+        [Required]
+        public int StatusId { get; set; }
         [Required,MaxLength(20)]
         public string StatusName { get; set; }
     }
