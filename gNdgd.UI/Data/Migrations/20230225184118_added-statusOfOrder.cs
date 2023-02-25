aa3d1e3ef8236e64385c38efc31ec1b6e6c45b6a
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-25 21:41:43 +0300

    Migration added

---
+++ gNdgd.UI/Data/Migrations/20230225184118_added-statusOfOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gNdgd.UI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedstatusOfOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "OrderStatuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "OrderStatuses");
+        }
+    }
+}
