1cda1b27554155d74f403fc3fd95c0fe7a98218d
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-25 08:53:46 +0300

    Add project files.

---
+++ gNdgd.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gNdgd.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
