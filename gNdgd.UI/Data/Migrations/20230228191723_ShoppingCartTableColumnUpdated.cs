7db81d5726b083834d57db6d2e9495d4295d8a4c
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-28 22:42:05 +0300

    migration added

---
+++ gNdgd.UI/Data/Migrations/20230228191723_ShoppingCartTableColumnUpdated.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gNdgd.UI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShoppingCartTableColumnUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserI",
+                table: "ShoppingCarts",
+                newName: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "ShoppingCarts",
+                newName: "UserI");
+        }
+    }
+}
