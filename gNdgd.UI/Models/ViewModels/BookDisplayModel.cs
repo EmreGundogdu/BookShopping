056d80501eb2dcc4533a394d9e734c0400bde25b
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-28 21:24:38 +0300

    BookDisplayModel added

---
+++ gNdgd.UI/Models/ViewModels/BookDisplayModel.cs
@@ -0,0 +1,8 @@
+﻿namespace gNdgd.UI.Models.ViewModels
+{
+    public class BookDisplayModel
+    {
+        public IEnumerable<Book>Books{ get; set; }
+        public IEnumerable<Genre>Genres{ get; set; }
+    }
+}
