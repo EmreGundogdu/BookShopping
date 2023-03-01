cbcd9301e93a0311cc136e3de175adcad538887b
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-03-01 06:57:29 +0300

    UsersOrder Controller added

---
+++ gNdgd.UI/Controllers/UserOrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gNdgd.UI.Controllers
+{
+    [Authorize]
+    public class UserOrderController : Controller
+    {
+        readonly IUserOrderRepository userOrderRepository;
+
+        public UserOrderController(IUserOrderRepository userOrderRepository)
+        {
+            this.userOrderRepository = userOrderRepository;
+        }
+
+        public async Task<ActionResult> UserOrders()
+        {
+            var orders = await userOrderRepository.UserOrders();
+            return View(orders);
+        }
+    }
+}
